356aaf0395b23575474c9db493a65cd0428d3227
Benny <benny.le890@gmail.com>
2022-09-09 07:48:56 -0400

    second commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,38 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is your project name?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project.'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => err ? console.log(err) : console.log('Readme Generated!'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            //do stuff with the answers
+            console.log(answers);
+
+            //after all answers are collected, write to file!
+            //TODO - build a string that uses our answers to generate some markdown, and write that to our file as the 'data' parameter. (replace the test values below)
+            writeToFile('test.txt', 'here is some data');
+        })
+}
 
 // Function call to initialize app
 init();
