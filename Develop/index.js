45329892a3238d01db96ef9a6662dda66fa2ddf9
Benny <benny.le890@gmail.com>
2022-09-11 21:30:09 -0400

    im not sure

--- Develop/index.js
+++ Develop/index.js
@@ -1,17 +1,65 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('.utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        //validate property to checkt hat the user provided a value
+        validate: (value)=>{ if(value){return true} else {return 'Must input value to continue'}},
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: (value)=>{ if(value){return true} else {return 'Must input value to continue'}},
+    },
+    {
+        type: 'input',
         name: 'projectName',
-        message: 'What is your project name?'
+        message: 'What is your project name?',
+        validate: (value)=>{ if(value){return true} else {return 'Must input value to continue'}},
     },
     {
         type: 'input',
         name: 'description',
-        message: 'Describe your project.'
+        message: 'Describe your project.',
+        validate: (value)=>{ if(value){return true} else {return 'Must input value to continue'}},
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'none']
+    },
+    {
+        type: 'input',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies?',
+        validate: (value)=>{ if(value){return true} else {return 'Must input value to continue'}},
+    },
+    {
+        type: 'input',
+        name: 'commands',
+        message: 'What command should be run to run tests?',
+        validate: (value)=>{ if(value){return true} else {return 'Must input value to continue'}},
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',
+        validate: (value)=>{ if(value){return true} else {return 'Must input value to continue'}},
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?',
+        validate: (value)=>{ if(value){return true} else {return 'Must input value to continue'}},
     },
 ];
 
@@ -30,7 +78,29 @@ function init() {
 
             //after all answers are collected, write to file!
             //TODO - build a string that uses our answers to generate some markdown, and write that to our file as the 'data' parameter. (replace the test values below)
-            writeToFile('test.txt', 'here is some data');
+            writeToFile('README.md', 'here is some data');
+
+            const template = `${projectName}
+
+            # ${data.projectName}
+            https://github.com/${data.github}/${data.projectName}
+            # Description
+            ${data.Description}
+            #Table of Contents
+            * [Installation](#installation)
+            * [Usage](#usage)
+            * [License](#license)
+            * [Contributing](#contributing)
+            # Installation
+            The following necessary dependencies must be installed to run the application
+            # Usage
+            In order to use this app, ${data.usage}
+            # License
+            This project is licensed under the ${data.license} license.
+            ![GitHub License](https://img.shields.io/badge/license-MIT-blue.svg)
+            # Contributing
+            Contributors: ${data.contributing}`
+    
         })
 }
 
